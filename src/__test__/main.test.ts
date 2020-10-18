e44992739bcff13be34bc1236e6e7cff504f8c80
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-10-18 10:36:43 +0900

    chore: move spec file

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,4 +1,4 @@
-import {wait} from '../src/wait'
+import {wait} from '../wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -19,7 +19,7 @@ test('wait 500 ms', async () => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '../..', 'dist', 'index.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
   }
