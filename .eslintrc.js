91cc9921cf8c54584a295c47e62689749734314f
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-10-18 16:42:15 +0900

    style: run lint
    false
    
    false

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,22 @@ module.exports = {
   env: {
     node: true,
     es6: true,
-    'jest/globals': true
+    "jest/globals": true,
   },
   parserOptions: {
     ecmaVersion: 9,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['jest', '@typescript-eslint', '@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["jest", "@typescript-eslint", "prettier"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  }
-}
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
