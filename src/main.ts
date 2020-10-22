9da6251c0e8fe5bec0f24a5660d000acbddd0e15
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-10-22 18:56:25 +0900

    PUJTIA-4 fix: fix ref env

--- src/main.ts
+++ src/main.ts
@@ -8,30 +8,14 @@ import * as core from '@actions/core';
 const isProduction = process.env.NODE_ENV === 'production';
 
 async function run(): Promise<void> {
-  // const JIRA_CLIENT_PROTOCOL = core.getInput('JIRA_CLIENT_PROTOCOL');
-  // const JIRA_CLIENT_HOST = core.getInput('JIRA_CLIENT_HOST');
-  // const JIRA_CLIENT_USERNAME = core.getInput('JIRA_CLIENT_USERNAME');
-  // const JIRA_CLIENT_PASSWORD = core.getInput('JIRA_CLIENT_PASSWORD');
-  // const JIRA_CLIENT_API_VERSION = core.getInput('JIRA_CLIENT_API_VERSION');
-  // const JIRA_CLIENT_STRICT_SSL =
-  //   core.getInput('JIRA_CLIENT_STRICT_SSL') === 'true';
+  const JIRA_CLIENT_PROTOCOL = core.getInput('JIRA_CLIENT_PROTOCOL');
 
-  const JIRA_CLIENT_PROTOCOL =
-    core.getInput('JIRA_CLIENT_PROTOCOL') || process.env.JIRA_CLIENT_PROTOCOL;
-  const JIRA_CLIENT_HOST =
-    core.getInput('JIRA_CLIENT_HOST') ||
-    (process.env.JIRA_CLIENT_HOST as string);
-  const JIRA_CLIENT_USERNAME =
-    core.getInput('JIRA_CLIENT_USERNAME') || process.env.JIRA_CLIENT_USERNAME;
-  const JIRA_CLIENT_PASSWORD =
-    core.getInput('JIRA_CLIENT_PASSWORD') || process.env.JIRA_CLIENT_PASSWORD;
-  const JIRA_CLIENT_API_VERSION =
-    core.getInput('JIRA_CLIENT_API_VERSION') ||
-    process.env.JIRA_CLIENT_API_VERSION;
-
+  const JIRA_CLIENT_HOST = core.getInput('JIRA_CLIENT_HOST');
+  const JIRA_CLIENT_USERNAME = core.getInput('JIRA_CLIENT_USERNAME');
+  const JIRA_CLIENT_PASSWORD = core.getInput('JIRA_CLIENT_PASSWORD');
+  const JIRA_CLIENT_API_VERSION = core.getInput('JIRA_CLIENT_API_VERSION');
   const JIRA_CLIENT_STRICT_SSL =
-    core.getInput('JIRA_CLIENT_STRICT_SSL') === 'true' ||
-    process.env.JIRA_CLIENT_STRICT_SSL === 'true';
+    core.getInput('JIRA_CLIENT_STRICT_SSL') === 'true';
 
   const jira = new Jira({
     protocol: JIRA_CLIENT_PROTOCOL,
