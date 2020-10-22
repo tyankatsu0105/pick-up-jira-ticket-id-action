8208fcb24dbdba01c10e826d373d8dc394377913
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-10-22 18:15:46 +0900

    PUJTIA-4 fix: fix env

--- src/main.ts
+++ src/main.ts
@@ -2,53 +2,44 @@ import { Jira } from './jira';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 import * as core from '@actions/core';
 // import { wait } from './wait';
 
 async function run(): Promise<void> {
-  const JIRA_CLIENT_PROTOCOL = core.getInput('JIRA_CLIENT_PROTOCOL');
-  const JIRA_CLIENT_HOST = core.getInput('JIRA_CLIENT_HOST');
-  const JIRA_CLIENT_USERNAME = core.getInput('JIRA_CLIENT_USERNAME');
-  const JIRA_CLIENT_PASSWORD = core.getInput('JIRA_CLIENT_PASSWORD');
-  const JIRA_CLIENT_API_VERSION = core.getInput('JIRA_CLIENT_API_VERSION');
-  const JIRA_CLIENT_STRICT_SSL =
-    core.getInput('JIRA_CLIENT_STRICT_SSL') === 'true';
-
-  // const JIRA_CLIENT_PROTOCOL =
-  //   (isProduction
-  //     ? core.getInput('JIRA_CLIENT_PROTOCOL')
-  //     : process.env.JIRA_CLIENT_PROTOCOL) || 'https';
-  // const JIRA_CLIENT_HOST =
-  //   (isProduction
-  //     ? core.getInput('JIRA_CLIENT_HOST')
-  //     : process.env.JIRA_CLIENT_HOST) || '';
-  // const JIRA_CLIENT_USERNAME =
-  //   (isProduction
-  //     ? core.getInput('JIRA_CLIENT_USERNAME')
-  //     : process.env.JIRA_CLIENT_USERNAME) || '';
-  // const JIRA_CLIENT_PASSWORD =
-  //   (isProduction
-  //     ? core.getInput('JIRA_CLIENT_PASSWORD')
-  //     : process.env.JIRA_CLIENT_PASSWORD) || '';
-  // const JIRA_CLIENT_API_VERSION =
-  //   (isProduction
-  //     ? core.getInput('JIRA_CLIENT_API_VERSION')
-  //     : process.env.JIRA_CLIENT_API_VERSION) || '2';
+  // const JIRA_CLIENT_PROTOCOL = core.getInput('JIRA_CLIENT_PROTOCOL');
+  // const JIRA_CLIENT_HOST = core.getInput('JIRA_CLIENT_HOST');
+  // const JIRA_CLIENT_USERNAME = core.getInput('JIRA_CLIENT_USERNAME');
+  // const JIRA_CLIENT_PASSWORD = core.getInput('JIRA_CLIENT_PASSWORD');
+  // const JIRA_CLIENT_API_VERSION = core.getInput('JIRA_CLIENT_API_VERSION');
   // const JIRA_CLIENT_STRICT_SSL =
-  //   ((isProduction
-  //     ? core.getInput('JIRA_CLIENT_API_STRICT_SSL')
-  //     : process.env.JIRA_CLIENT_API_STRICT_SSL) === '' &&
-  //     true) ||
-  //   ((isProduction
-  //     ? core.getInput('JIRA_CLIENT_API_STRICT_SSL')
-  //     : process.env.JIRA_CLIENT_API_STRICT_SSL) === 'true' &&
-  //     true) ||
-  //   ((isProduction
-  //     ? core.getInput('JIRA_CLIENT_API_STRICT_SSL')
-  //     : process.env.JIRA_CLIENT_API_STRICT_SSL) === 'false' &&
-  //     false);
+  //   core.getInput('JIRA_CLIENT_STRICT_SSL') === 'true';
+
+  const JIRA_CLIENT_PROTOCOL =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_PROTOCOL')
+      : process.env.JIRA_CLIENT_PROTOCOL) || 'https';
+  const JIRA_CLIENT_HOST =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_HOST')
+      : process.env.JIRA_CLIENT_HOST) || '';
+  const JIRA_CLIENT_USERNAME =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_USERNAME')
+      : process.env.JIRA_CLIENT_USERNAME) || '';
+  const JIRA_CLIENT_PASSWORD =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_PASSWORD')
+      : process.env.JIRA_CLIENT_PASSWORD) || '';
+  const JIRA_CLIENT_API_VERSION =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_API_VERSION')
+      : process.env.JIRA_CLIENT_API_VERSION) || '2';
+  const JIRA_CLIENT_STRICT_SSL =
+    (isProduction
+      ? core.getInput('JIRA_CLIENT_API_STRICT_SSL') === 'true'
+      : process.env.JIRA_CLIENT_API_STRICT_SSL) === 'true' && true;
 
   const jira = new Jira({
     protocol: JIRA_CLIENT_PROTOCOL,
